8a3c8ac3ec3d5d96f3cd25ba3c81bc0ab26f594a
Blueheart65 <nbutler728@boyslatin.net>
2026-09-01 15:10:52 +0000

    datatypes

---
+++ unit1/datatypes.py
@@ -0,0 +1,30 @@
+# String - charachers that are wrapped within quotes.
+# charachers can be letters, numbers, and symbols.
+# you can use both single and double quotation
+# marks for strings
+
+"Nyere butler"
+'123 Main Street'
+'this cost $100 dollars'
+# the hashtag is a maker that makes anything written
+# behind it invisible to the computor
+
+#integers- dtatypes that are just whole numbers 
+# or numbers without a decimal point. we dont have to add any special symbols we can write the number as is.
+
+10
+30
+500000
+5657658
+
+#floats- Anumber with a decimal 
+4.99
+6.893
+0.23
+
+#Boolean- a data type that can only either true or false we do not use quotatrion marks for boolean values.
+# the computorunderstand these words as something unique.
+
+false
+false 
+true 
