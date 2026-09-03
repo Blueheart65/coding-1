1ceb787291f12e564b4c1c7986d87d0651bc7339
Blueheart65 <nbutler728@boyslatin.net>
2026-09-03 15:04:45 +0000

    added variables complying to variable rules

---
+++ unit1/variableActivity.py
@@ -0,0 +1,28 @@
+#variables-a container that stores data. 
+# you can name variables almost anything.
+apple = 1.00
+orange= 1.10
+fire cracker = 4.89
+direwolfswordcount = 45
+direwolfswordprice = 34.56
+direwolfswordSKU = '787588939479'
+
+#When we want to create a variable, we make up a word, use the equal signthen assign it a datatype.
+# Whenever the com[utpor see our varibale word it will always use the data types
+
+# variable naming rules
+# A variable must start with lettrs and cannot start with numbers 
+# A variable can be mixed of letters and numbers But not symbols 
+# a variable cannot be a pyton keyword 
+# Variables with multiple words do not have spaces 
+
+
+AppleAmount = 73
+StudentsinMathClass = 18
+travelinsurance = "yes or no"
+Gymembershipbronze = 4.99
+Gymmembershipsilver = 8.50
+Gymembershipgold = 10.99
+Grapesoda = 1.51
+applejuice =1.29
+Vodka = 10.48
