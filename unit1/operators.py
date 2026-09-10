e16c041ee5de3f8ced7f7909818671022eb30ced
Blueheart65 <nbutler728@boyslatin.net>
2026-09-10 15:18:51 +0000

    operators

---
+++ unit1/operators.py
@@ -0,0 +1,37 @@
+#assignment operators- Exclusivly used to assain values to veriables. 
+
+# We use a single equal sign  to represents the assignment operator
+name = Nyere
+Grade = 11
+
+# Arithemetic Operators- used on numerical data stypes to perform conclutiond
+# ingetgers and floats 
+print (4+4) #oddional operator 
+
+# comparison operators- set of symbols used to asses if date is the same or different and how they differ
+
+print (10 > 2)
+# 2 equal signs compare if something is the same
+
+print("book" == "Book") # same as (false)
+print(2.0 == 2) #same as (true)
+
+# not equal is written with !=
+# this is the checl and filter for values that are not the same
+# Exclamation always means not in programming 
+print(22!= 100) # True- here are not the same 
+Print(300 != 300) # false these are the same
+
+
+# logical opertors- compares 2 conditiond to check if they are true or false 
+# conditions = other operators 
+# instead of symbols, we represent thes tith words: and or, not
+
+# And-checks if 2 conditions are true, if yes the final result is true 
+print (3 > 1 and 100 > 50) # this would come out to be true 
+
+# OR- checks if only 1 condition id true. if yes, the final result will be true 
+Print (3 > 1 or 100 -- 50) 
+
+# NOT - the "oppisite day" operator. it will reverse the result of the logical operators.
+print(not(3 > 1 and 100 > 50))
